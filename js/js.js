e97b4cfb3a3d4596bfcd2dc29a3e1ec284d10ea2
Fazlul Alam <fazlulalam619@gmail.com>
2024-03-04 22:46:26 +0600

    Complete Latest Post Section

---
+++ js/js.js
@@ -0,0 +1,47 @@
+const latestPost = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
+  );
+  const data = await res.json();
+  // console.log(post);
+  displayLatestPost(data);
+};
+const displayLatestPost = (data) => {
+  console.log(data);
+  const showAllContainer = document.getElementById("post-container");
+
+  data.forEach((post) => {
+    const postCard = document.createElement("div");
+    postCard.classList = `card w-96 bg-base-100 shadow-xl `;
+    postCard.innerHTML = `
+            <figure>
+              <img
+                src="${post.cover_image}"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="mt-3 flex gap-2 pl-4">
+              <img src="images/Frame.png" alt="" />
+              <p>${post?.author?.posted_date || "No published Date"}</p>
+            </div>
+            <div class="card-body">
+              <h2 class="card-title">${post.title}
+              </h2>
+              <p>${post.description}
+              </p>
+              <div class="flex gap-2">
+                <img class="w-12 h-12 rounded-full" src="${
+                  post.profile_image
+                }" alt="" />
+                <div>
+                  <h1 class="font-semibold">${post.author.name}</h1>
+                  <p>${post?.author?.designation || "Unknown"}</p>
+                </div>
+              </div>
+            </div>
+          
+    `;
+    showAllContainer.appendChild(postCard);
+  });
+};
+latestPost();
